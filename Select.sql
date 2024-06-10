95689ebde98152c9bcceafe4281ecca0ccecf977
Vladimir Simachev aka Akadza322 <tumanov240360@mail.ru>
2024-06-10 20:02:11 +0300

    Initial commit

---
+++ Select.sql
@@ -0,0 +1,49 @@
+-- ЗАДАНИЕ 2
+-- 1) Название и продолжительность самого длительного трека.
+select "name", duration from tracks
+where duration = (select max(duration) from tracks);
+
+-- 2) Название треков, продолжительность которых не менее 3,5 минут.
+select "name", duration from tracks
+where duration > 3.5;
+
+-- 3) Названия сборников, вышедших в период с 2018 по 2020 год включительно.
+select "name", release_years from collection
+where release_years > '01.01.2018' and release_years < '31.12.2020';
+
+-- 4) Исполнители, чьё имя состоит из одного слова.
+select "name" from band  
+where "name" not like '% %';
+
+-- 5) Название треков, которые содержат слово «мой» или «my».
+select "name" from tracks  
+where "name" like '%My%';
+
+-- ЗАДАНИЕ 3
+-- 1) Количество исполнителей в каждом жанре.
+select s."name" , count(sb.band_id) from style_band sb
+join "style" s on sb.style_id = s.style_id
+group by s."name";
+
+-- 2) Количество треков, вошедших в альбомы 2019–2020 годов.
+select count(t.name) from tracks t, album a 
+where t.album_id = a.album_id and release_years >= '01.01.2019' and release_years <= '31.12.2020'
+
+-- 3) Средняя продолжительность треков по каждому альбому.
+select a."name", avg(t.duration) from album a, tracks t 
+where t.album_id = a.album_id 
+group by a."name"
+
+-- 4) Все исполнители, которые не выпустили альбомы в 2020 году.
+select b."name", a.release_years from band b, album a, feat f 
+where EXTRACT(YEAR FROM a.release_years) != 2020 and b.band_id = f.band_id and a.album_id = f.album_id 
+
+-- 5) Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+select c."name" from collection c
+join collection_create cc on c.collection_id = cc.collection_id
+join tracks t on cc.track_id = t.track_id 
+join album a on t.album_id = a.album_id
+join feat f on a.album_id = f.album_id 
+join band b on f.band_id = b.band_id
+where b."name" like '%NF%' 
+group by c."name" 
---
+++ create.sql
@@ -0,0 +1,51 @@
+create table if not exists Band (
+	band_id int primary key,
+	name text not null
+);
+
+create table if not exists Style (
+	style_id int primary key,
+	name text not null
+);
+
+create table if not exists style_band (
+	band_id int references Band(band_id),
+	style_id int references Style(style_id),
+	
+	constraint band_style_pkey primary key (band_id, style_id) 
+);
+
+create table if not exists Album (
+	album_id int primary key,
+	name text not null,
+	band_id int references Band(band_id),
+	release_years date not null
+);
+
+create table if not exists Tracks(
+	track_id int primary key,
+	name text not null,
+	duration numeric not null,
+	album_id int references Album(album_id)
+);
+
+create table if not exists Feat(
+	band_id int references Band(band_id),
+	album_id int references Album(album_id),
+	
+	constraint band_album_pkey primary key (band_id, album_id)
+);
+
+create table if not exists Collection(
+	collection_id int primary key,
+	name text not null,
+	release_years date not null
+);
+
+create table if not exists Collection_create(
+	collection_id int references Collection(collection_id),
+	album_id int references Album(album_id),
+	track_id int references Tracks(track_id),
+	
+	constraint album_track_collection primary key (album_id, track_id, collection_id)
+);
